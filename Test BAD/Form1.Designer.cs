c8530bd8bdc27db6ce49e30692a9713048e0386e
Rostislav Fedoriv <froizerner@gmail.com>
2023-01-29 11:24:08 +0200

    Add project files.

---
+++ Test BAD/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Test_BAD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.InputTextBox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ResultButton = new System.Windows.Forms.Button();
+            this.ResultTextBox = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.ResultTextBox);
+            this.panel1.Controls.Add(this.ResultButton);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.InputTextBox);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 426);
+            this.panel1.TabIndex = 0;
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(3, 46);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(311, 377);
+            this.InputTextBox.TabIndex = 0;
+            this.InputTextBox.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Window;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label1.Location = new System.Drawing.Point(3, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Input Text";
+            // 
+            // ResultButton
+            // 
+            this.ResultButton.BackColor = System.Drawing.SystemColors.Window;
+            this.ResultButton.Location = new System.Drawing.Point(204, 14);
+            this.ResultButton.Name = "ResultButton";
+            this.ResultButton.Size = new System.Drawing.Size(110, 29);
+            this.ResultButton.TabIndex = 2;
+            this.ResultButton.Text = "Result";
+            this.ResultButton.UseVisualStyleBackColor = false;
+            this.ResultButton.Click += new System.EventHandler(this.ResultButton_Click);
+            // 
+            // ResultTextBox
+            // 
+            this.ResultTextBox.Location = new System.Drawing.Point(462, 46);
+            this.ResultTextBox.Name = "ResultTextBox";
+            this.ResultTextBox.ReadOnly = true;
+            this.ResultTextBox.Size = new System.Drawing.Size(311, 377);
+            this.ResultTextBox.TabIndex = 3;
+            this.ResultTextBox.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Window;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label2.Location = new System.Drawing.Point(466, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 26);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox ResultTextBox;
+        private System.Windows.Forms.Button ResultButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox InputTextBox;
+    }
+}
+
---
+++ Test BAD/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Test_BAD
+{
+    public partial class Form1 : Form
+    {
+        private string InputText { get; set; }
+        private List<char> FirstSymbols { get; set; } = new List<char>();
+        public Form1()
+        {
+            InitializeComponent();
+            InputTextBox.Text += "The Tao gave birth to machine language.  Machine language gave birth\r\nto the assembler.\r\nThe assembler gave birth to the compiler.  Now there are ten thousand\r\nlanguages.\r\nEach language has its purpose, however humble.  Each language\r\nexpresses the Yin and Yang of software.  Each language has its place within\r\nthe Tao.\r\nBut do not program in COBOL if you can avoid it.\r\n        -- Geoffrey James, \"The Tao of Programming\"";
+        }
+
+        private void ResultButton_Click(object sender, System.EventArgs e)
+        {
+            ResultTextBox.Text = "";
+            FirstSymbols = new List<char>();
+            try
+            {
+            InputText = InputTextBox.Text;
+
+            Regex trimmer = new Regex(@"\s\s+");
+            InputText = trimmer.Replace(InputText, " ");
+
+            var splitedWords = InputText
+                .Split()
+                .Select(x => x
+                             .Trim(InputText
+                             .Where(Char.IsPunctuation)
+                             .Distinct()
+                             .ToArray())).ToList();
+            splitedWords.ForEach(x => x.Replace(".", string.Empty));
+            splitedWords.RemoveAll(x=>x=="");
+            splitedWords.ForEach(f => FirstSymbols.Add(f.GroupBy(x => x).FirstOrDefault(g => g.Count() == 1).Key));
+            ResultTextBox.Text += FirstSymbols.GroupBy(x => x).FirstOrDefault(f=>f.Count()==1).Key;
+            Console.WriteLine();
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
